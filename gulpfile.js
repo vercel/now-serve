e4bd554cec075b46f7a382eba1e455becc9c5c9f
Leonard Lamprecht <mindrun@icloud.com>
2016-04-26 09:53:56 +0200

    Build properly

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp')
 const babel = require('gulp-babel')
 const cache = require('gulp-cached')
+const chmod = require('gulp-chmod')
 
 const path = 'bin/**/*'
 
@@ -8,6 +9,7 @@ gulp.task('transpile', () => {
   return gulp.src(path)
   .pipe(cache('transpile'))
   .pipe(babel())
+  .pipe(chmod(755))
   .pipe(gulp.dest('dist'))
 })
 
